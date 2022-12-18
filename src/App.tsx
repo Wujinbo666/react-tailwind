a218fcd6fdc75209cbb6a0d92562f31a56d8645a
bigfacebobo <1136046731@qq.com>
2022-12-19 07:16:08 +0800

    firstcommit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Index from './pages'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Index/>
+  )
+}
+
+export default App
---
+++ src/widgets/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='grid justify-items-start gap-8 pb-28 relative'>
+      <p className='text-4xl md:text-6xl font-black text-center leading-normal  md:leading-normal'>
+        打造一个完美的、现象级的、充满创造力的网站
+      </p>
+    </div>
+  )
+}
+
+export default Hero
